8421a670ef1b035c0dcc4b11a9d9ebed85897e3c
Fraser Scott-Morrison <fraserscottmorrison@me.com>
2017-02-13 16:20:41 +1300

    DesignableButton first beta

---
+++ DesignableButton.podspec
@@ -0,0 +1,25 @@
+Pod::Spec.new do |s|
+  s.name             = "DesignableButton"
+  s.version          = "0.1"
+  s.summary          = "A Custom UIButton with Centralised Reusable UIButton Styles, Powered by Interface Builder"
+
+  s.description      = <<-DESC
+                       DesignableButton allows you to declare reusable button style definitions. In Interface Builder you can easily choose these style for your DesignableButtons and Interface Builder will render these styles immediately.
+
+Common button styles, such as corner radius, are also made available in Interface Builder
+                       DESC
+
+  s.homepage         = "https://github.com/IdleHandsApps/DesignableButton/"
+  s.license          = { :type => "MIT" }
+
+  s.author           = { "Fraser Scott-Morrison" => "fraserscottmorrison@me.com" }
+
+  s.ios.deployment_target = "8.0"
+
+  s.source           = { :git => "https://github.com/IdleHandsApps/DesignableButton", :tag => s.version.to_s }
+
+  s.source_files = "Sources/*.swift"
+
+  s.framework       = "UIKit"
+  s.requires_arc    = true
+end
---
+++ Sources/DesignableButton.swift
@@ -0,0 +1,138 @@
+//
+//  DesignableButton.swift
+//  Neighbourly
+//
+//  Created by Fraser on 8/02/17.
+//  Copyright © 2017 Neighbourly Ltd. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableButton: UIButton {
+    
+    public func getStyles() -> [String: (DesignableButton) -> Void] {
+        return DesignableButton.styles
+    }
+
+    public static var styles = ["": { (designableButton: DesignableButton) -> Void in
+        // default "none" style
+        if designableButton.isHighlighted || designableButton.isSelected {
+            designableButton.backgroundColor = designableButton.selectedColor
+        } else if designableButton.isEnabled {
+            designableButton.backgroundColor = designableButton.defaultColor
+        }
+        else {
+            designableButton.backgroundColor = designableButton.disabledColor
+        }
+        designableButton.layer.cornerRadius = designableButton.cornerRadius ?? 0
+        designableButton.layer.borderWidth = designableButton.borderWidth ?? 0
+        designableButton.layer.borderColor = designableButton.borderColor?.cgColor
+        }, "primary": { (designableButton: DesignableButton) -> Void in
+            if designableButton.isHighlighted || designableButton.isSelected {
+                designableButton.backgroundColor = designableButton.selectedColor ?? ColorNeighbourly.blueSelected()
+            } else if designableButton.isEnabled {
+                designableButton.backgroundColor = designableButton.defaultColor ?? ColorNeighbourly.blue()
+            }
+            else {
+                designableButton.backgroundColor = designableButton.disabledColor ?? ColorNeighbourly.grayLight()
+            }
+            
+            designableButton.setTitleColor(UIColor.white, for: .normal)
+            designableButton.layer.cornerRadius = designableButton.cornerRadius ?? 5
+            designableButton.layer.borderWidth = designableButton.borderWidth ?? 0
+            designableButton.layer.borderColor = designableButton.borderColor != nil ? designableButton.borderColor!.cgColor : nil
+        }]   // = [String: ((DesignableButton)->Void)]()
+
+    
+    /*public static func setStyle(style: @escaping (DesignableButton) -> Void, for key: String) {
+        self.styles.updateValue(style, forKey: key.lowercased())
+    }*/
+    
+    public func setStyleForAll() {
+        self.reversesTitleShadowWhenHighlighted = false
+        self.showsTouchWhenHighlighted = false
+        self.adjustsImageWhenHighlighted = false
+    }
+    
+    /*public static var styleForAll: ((DesignableButton)->Void) = {(designableButton: DesignableButton) -> Void in
+        designableButton.reversesTitleShadowWhenHighlighted = false
+        designableButton.showsTouchWhenHighlighted = false
+        designableButton.adjustsImageWhenHighlighted = false
+    }*/
+
+    @IBInspectable open var buttonStyle: String = "" {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    
+    @IBInspectable open var defaultColor: UIColor? {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    @IBInspectable open var selectedColor: UIColor? {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    @IBInspectable open var disabledColor: UIColor? {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    
+    @IBInspectable open var cornerRadius: CGFloat? {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    @IBInspectable open var borderWidth: CGFloat? {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    @IBInspectable open var borderColor: UIColor? {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.updateStyles()
+    }
+    
+    open override var isHighlighted: Bool {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    open override var isSelected: Bool {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    open override var isEnabled: Bool {
+        didSet {
+            self.updateStyles()
+        }
+    }
+    
+    func updateStyles() {
+        
+        if self.buttonStyle.characters.count > 0 {
+            self.setStyleForAll()
+        }
+        
+        if let styleBlock = self.getStyles()[self.buttonStyle] as ((DesignableButton)->Void)! {
+            styleBlock(self)
+        }
+        
+        layer.masksToBounds = layer.cornerRadius > 0
+        
+        assert(self.buttonType == UIButtonType.custom, "Designable Button \"\(self.titleLabel?.text)\" buttonType must be Custom") // here
+    }
+}
