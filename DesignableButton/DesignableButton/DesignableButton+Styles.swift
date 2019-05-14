67d639b291712e9f868d2333d464d46221276472
Fraser <fraser@ignitiondigital.co.nz>
2019-05-15 10:02:25 +1200

    Removing warnings

--- DesignableButton/DesignableButton/DesignableButton+Styles.swift
+++ DesignableButton/DesignableButton/DesignableButton+Styles.swift
@@ -22,19 +22,19 @@ extension DesignableButton {
         DesignableButton.setStyle(style: { (designableButton: DesignableButton) -> Void in
             if designableButton.isHighlighted || designableButton.isSelected {
                 designableButton.setTitleColor(designableButton.customTextColor ?? UIColor.white, for: UIControl.State())
-                designableButton.backgroundColor = designableButton.selectedColor ?? Color.redSelected()
-                designableButton.layer.borderColor = designableButton.selectedColor?.cgColor ?? Color.redSelected().cgColor
+                designableButton.backgroundColor = designableButton.selectedColor ?? Color.redSelected
+                designableButton.layer.borderColor = designableButton.selectedColor?.cgColor ?? Color.redSelected.cgColor
                 designableButton.layer.borderWidth = designableButton.borderWidth ?? 0
             } else if designableButton.isEnabled {
                 designableButton.setTitleColor(designableButton.customTextColor ?? UIColor.white, for: UIControl.State())
-                designableButton.backgroundColor = designableButton.defaultColor ?? Color.red()
-                designableButton.layer.borderColor = designableButton.defaultColor?.cgColor ?? Color.red().cgColor
+                designableButton.backgroundColor = designableButton.defaultColor ?? Color.red
+                designableButton.layer.borderColor = designableButton.defaultColor?.cgColor ?? Color.red.cgColor
                 designableButton.layer.borderWidth = designableButton.borderWidth ?? 0
             }
             else {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.gray(), for: UIControl.State())
-                designableButton.backgroundColor = designableButton.disabledColor ?? Color.grayLightest()
-                designableButton.layer.borderColor = designableButton.borderColor?.cgColor ??  Color.gray().cgColor
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.gray, for: UIControl.State())
+                designableButton.backgroundColor = designableButton.disabledColor ?? Color.grayLightest
+                designableButton.layer.borderColor = designableButton.borderColor?.cgColor ??  Color.gray.cgColor
                 designableButton.layer.borderWidth = designableButton.borderWidth ?? 1
             }
             designableButton.setTitle(designableButton.titleLabel?.text, for: .normal)
@@ -46,41 +46,41 @@ extension DesignableButton {
         DesignableButton.setStyle(style: { (designableButton: DesignableButton) -> Void in
             if designableButton.isHighlighted || designableButton.isSelected {
                 designableButton.setTitleColor(designableButton.customTextColor ?? UIColor.white, for: UIControl.State())
-                designableButton.backgroundColor = designableButton.selectedColor ?? Color.redSelected()
+                designableButton.backgroundColor = designableButton.selectedColor ?? Color.redSelected
             } else if designableButton.isEnabled {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.red(), for: UIControl.State())
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.red, for: UIControl.State())
                 designableButton.backgroundColor = designableButton.defaultColor ?? UIColor.white
             }
             else {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.grayLight(), for: UIControl.State())
-                designableButton.backgroundColor = designableButton.disabledColor ?? Color.grayLightest()
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.grayLight, for: UIControl.State())
+                designableButton.backgroundColor = designableButton.disabledColor ?? Color.grayLightest
             }
             designableButton.setTitle(designableButton.titleLabel?.text, for: .normal)
             
             designableButton.layer.cornerRadius = designableButton.cornerRadius ?? 12
             designableButton.layer.borderWidth = designableButton.borderWidth ?? 1
-            designableButton.layer.borderColor = designableButton.borderColor != nil ? designableButton.borderColor!.cgColor : Color.red().cgColor
+            designableButton.layer.borderColor = designableButton.borderColor != nil ? designableButton.borderColor!.cgColor : Color.red.cgColor
         }, for: "secondary")
         
         DesignableButton.setStyle(style: { (designableButton: DesignableButton) -> Void in
             if designableButton.isHighlighted {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.redSelected(), for: UIControl.State())
-                designableButton.backgroundColor = designableButton.selectedColor ?? Color.red()
-                designableButton.tintColor = Color.redSelected()
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.redSelected, for: UIControl.State())
+                designableButton.backgroundColor = designableButton.selectedColor ?? Color.red
+                designableButton.tintColor = Color.redSelected
             }
             else if designableButton.isSelected {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.redSelected(), for: UIControl.State())
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.redSelected, for: UIControl.State())
                 designableButton.backgroundColor = designableButton.selectedColor ?? UIColor.clear
-                designableButton.tintColor = Color.redSelected()
+                designableButton.tintColor = Color.redSelected
             } else if designableButton.isEnabled {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.red(), for: UIControl.State())
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.red, for: UIControl.State())
                 designableButton.backgroundColor = designableButton.defaultColor ?? UIColor.clear
-                designableButton.tintColor = Color.red()
+                designableButton.tintColor = Color.red
             }
             else {
-                designableButton.setTitleColor(designableButton.customTextColor ?? Color.grayLight(), for: UIControl.State())
+                designableButton.setTitleColor(designableButton.customTextColor ?? Color.grayLight, for: UIControl.State())
                 designableButton.backgroundColor = designableButton.disabledColor ?? UIColor.clear
-                designableButton.tintColor = Color.grayLight()
+                designableButton.tintColor = Color.grayLight
             }
             
             if isInterfaceBuilder {
